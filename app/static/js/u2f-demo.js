dc6d8ffe32c6c4d31ef412f6581cea4c145cdbd6
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-05-23 17:05:53 +1200

    Added u2f libs. Fixed font loading over HTTP. Basic reg template

---
+++ app/static/js/u2f-demo.js
@@ -0,0 +1,45 @@
+
+var getJSON = function(url, callback){
+    fetch(url).then(function(response) {
+        return response.json()
+    }).then(function(json) {
+        callback(json)
+    }).catch(function(ex) {
+        callback({ 'error': ex })
+    })
+}
+
+var post = function (url, body, callback) {
+    fetch(url, {
+        method  : 'post',
+        headers : {
+            'Accept'       : 'application/json',
+            'Content-Type' : 'application/json'
+        },
+        body    : JSON.stringify(body)
+    }).then(function (response) {
+        return response.json()
+    }).then(function (json) {
+        callback(json)
+    }).catch(function (ex) {
+        callback({ 'error': ex })
+    })
+}
+
+
+var processForm = function(e) {
+    if (e.preventDefault) e.preventDefault();
+
+    console.log(form.elements['username'].value)
+    console.log(form.elements['password'].value)
+
+    console.log(getJSON('/enroll', (data) => console.log(data)))
+    // You must return false to prevent the default form behavior
+    return false;
+}
+
+var form = document.getElementById('u2f-reg-form');
+if (form.attachEvent)
+    form.attachEvent("submit", processForm);
+else
+    form.addEventListener("submit", processForm);
