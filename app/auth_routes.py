1d8fe9151829a4facfd9b71e791d95220b50339f
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-05-23 17:07:26 +1200

    Added islogged and u2f enroll routes

--- app/auth_routes.py
+++ app/auth_routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import jsonify, request
+from flask import jsonify, request, session
 
 users = {}
 
@@ -8,15 +8,16 @@ def register():
     """User login/authentication/session management."""
 
     if request.method == 'POST':
-        if 'username' not in request.form or 'password' not in request.form:
+        req = request.json
+        if 'username' not in req or 'password' not in req:
             return jsonify({'status': 'failed', 'error': 'Username or/and password missing'})
 
-        if request.form['username'] in users:
+        if req['username'] in users:
             return jsonify({'status': 'failed', 'error': 'User already exists'})
         else:
-            users[request.form['username']] = {
-                'username': request.form['username'],
-                'password': request.form['password']
+            users[req['username']] = {
+                'username': req['username'],
+                'password': req['password']
             }
             return jsonify({'status': 'success'})
 
@@ -25,12 +26,18 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        if 'username' not in request.form or 'password' not in request.form:
+        req = request.json
+        if 'username' not in req or 'password' not in req:
             return jsonify({'status': 'failed', 'error': 'Username or/and password missing'})
 
-        if request.form['username'] in users:
-            if users[request.form['username']]['password'] == request.form['username']:
+        if users.get(req['username']):
+            if users[req['username']]['password'] == req['username']:
+                print(session)
+
                 session['logged_in'] = True
+                session['username']  = req['username']
+
+                print(session)
                 return jsonify({'status': 'success'})
             else:
                 return jsonify({'status': 'failed', 'error': 'Username or/and password is incorrect'})
@@ -39,12 +46,26 @@ def login():
 
     return jsonify({})
     
-@app.route('/logout')
-def logout():
-    """User logout/authentication/session management."""
-    session.pop('logged_in', None)
-    return jsonify({'status': 'success'})
+@app.route('/islogged')
+def isLogged():
+    print(session)
 
-@app.route('/testjson')
-def test():
-    return jsonify({'hello': 'world'})
+    return jsonify({'logged_in': session.get('logged_in', False)})
+
+
+@app.route('/enroll')
+def u2fenroll():
+    print(users, session)
+
+    if not session.get('logged_in', False):
+        return jsonify({'status': 'failed', 'error': 'Access denied. You must login'})
+
+
+    print(users)
+    user = users[session['username']]
+
+    devices = [DeviceRegistration.wrap(device)
+               for device in user.get('_u2f_devices_', [])]
+    enroll = start_register(app.config['APPID'], devices)
+    user['_u2f_enroll_'] = enroll.json
+    return enroll.json
