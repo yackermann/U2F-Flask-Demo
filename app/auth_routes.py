1ea9536f724a2d870adb90da530407c29c333c5d
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-05-23 21:07:11 +1200

    Added logout function. Imported accidentally deleted u2f libs.

--- app/auth_routes.py
+++ app/auth_routes.py
@@ -5,7 +5,7 @@ users = {}
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    """User login/authentication/session management."""
+    """User register/authentication/session management."""
 
     if request.method == 'POST':
         req = request.json
@@ -25,19 +25,16 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """User login/authentication/session management."""
     if request.method == 'POST':
         req = request.json
-        if 'username' not in req or 'password' not in req:
+        if not req.get('username')  or not req.get('password'):
             return jsonify({'status': 'failed', 'error': 'Username or/and password missing'})
 
         if users.get(req['username']):
             if users[req['username']]['password'] == req['username']:
-                print(session)
-
                 session['logged_in'] = True
                 session['username']  = req['username']
-
-                print(session)
                 return jsonify({'status': 'success'})
             else:
                 return jsonify({'status': 'failed', 'error': 'Username or/and password is incorrect'})
@@ -48,24 +45,33 @@ def login():
     
 @app.route('/islogged')
 def isLogged():
-    print(session)
-
+    """User islogged/authentication/session management."""
     return jsonify({'logged_in': session.get('logged_in', False)})
 
 
+
+@app.route('/logout')
+def logout():
+    """User logout/authentication/session management."""
+    session.pop('logged_in', None)
+    session.pop('username', None)
+    return jsonify({'status': 'success'})
+
+
+from u2flib_server.jsapi import DeviceRegistration
+from u2flib_server.u2f import (start_register, complete_register, start_authenticate, verify_authenticate)
+from cryptography.hazmat.primitives.serialization import Encoding
+
 @app.route('/enroll')
 def u2fenroll():
-    print(users, session)
+    """User enroll/u2f/authentication/session management."""
 
     if not session.get('logged_in', False):
         return jsonify({'status': 'failed', 'error': 'Access denied. You must login'})
 
-
-    print(users)
     user = users[session['username']]
 
-    devices = [DeviceRegistration.wrap(device)
-               for device in user.get('_u2f_devices_', [])]
-    enroll = start_register(app.config['APPID'], devices)
+    devices = [DeviceRegistration.wrap(device) for device in user.get('_u2f_devices_', [])]
+    enroll  = start_register(app.config['APPID'], devices)
     user['_u2f_enroll_'] = enroll.json
     return enroll.json
