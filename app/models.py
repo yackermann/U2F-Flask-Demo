ac2bddadb0fa1ecd1e6ce218fe97d2d922d33a9b
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-05-24 17:26:24 +1200

    models.Auth: Added exists method

--- app/models.py
+++ app/models.py
@@ -1,15 +1,19 @@
 from app import db
+from sqlalchemy.sql.expression import select, exists
 
 class Auth(db.Model):
     __tablename__ = 'users'
 
     id       = db.Column(db.Integer, primary_key= True)
-    username = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(70))
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
 
-    def __init__(self, name=None, password=None):
-        self.name = name
+    def __init__(self, username=None, password=None):
+        self.username = username
         self.password = password
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<User %r>' % (self.username)
+
+    def exists(self):
+        return db.session.query(exists().where(Auth.username == self.username)).scalar()
