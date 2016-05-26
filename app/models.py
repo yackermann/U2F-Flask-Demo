00db484d53e012e0d52ffc402b2e3fc1061a6032
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-05-26 21:57:29 +1200

    Added U2F methods. Added self saving commit() method.

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from app import db, passwords
 from sqlalchemy.sql.expression import select, exists
-# from passwords import hash_password, check_password 
+import json
 
 class Auth(db.Model):
     __tablename__ = 'users'
 
-    id       = db.Column(db.Integer, primary_key= True)
-    username = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
+    id          = db.Column(db.Integer, primary_key= True)
+    username    = db.Column(db.String , unique=True)
+    password    = db.Column(db.String)
+    u2f_devices = db.Column(db.String)
 
     def __init__(self, username, password=None):
         self.username = username
@@ -15,14 +16,30 @@ class Auth(db.Model):
         if password:
             self.password = passwords.hash_password(password)
 
+        self.u2f_devices = json.dumps([])
+
     def __repr__(self):
         return '<User %r>' % (self.username)
+
+    def commit(self):
+        db.session.add(self)
+        db.session.commit()
 
     def set_password(self, password):
         """Generate a random salt and return a new hash for the password."""
         self.password = passwords.hash_password(password)
 
-
     def check_password(self, password):
         """Check a password against an existing hash."""
         return passwords.check_password(self.password, password)
+
+
+
+    # U2F
+    def get_u2f_devices(self):
+        """Returns U2F devices"""
+        return json.loads(self.u2f_devices)
+
+    def set_u2f_devices(self, devices):
+        """Saves U2F devices"""
+        self.u2f_devices = json.dumps(devices)
