1931db6be7d715f8fea037bae1414779fb545a0a
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-05-26 20:22:09 +1200

    Removed old ssl config

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-# from OpenSSL import SSL
-# context = SSL.Context(SSL.SSLv23_METHOD)
-# context.use_privatekey_file('domain.key')
-# context.use_certificate_file('domain.crt')
-
 from app import app
 context = ('domain.crt', 'domain.key')
 app.run(debug=True, ssl_context=context)
