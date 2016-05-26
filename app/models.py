e759faa6d1a0f3bd48ab5fad83ac9456d10c91f1
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-05-26 20:25:00 +1200

    Added password hashing

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from app import db
+from app import db, passwords
 from sqlalchemy.sql.expression import select, exists
+# from passwords import hash_password, check_password 
 
 class Auth(db.Model):
     __tablename__ = 'users'
@@ -8,12 +9,20 @@ class Auth(db.Model):
     username = db.Column(db.String, unique=True)
     password = db.Column(db.String)
 
-    def __init__(self, username=None, password=None):
+    def __init__(self, username, password=None):
         self.username = username
-        self.password = password
+
+        if password:
+            self.password = passwords.hash_password(password)
 
     def __repr__(self):
         return '<User %r>' % (self.username)
 
-    def exists(self):
-        return db.session.query(exists().where(Auth.username == self.username)).scalar()
+    def set_password(self, password):
+        """Generate a random salt and return a new hash for the password."""
+        self.password = passwords.hash_password(password)
+
+
+    def check_password(self, password):
+        """Check a password against an existing hash."""
+        return passwords.check_password(self.password, password)
