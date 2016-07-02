9729416bcecc84c49c737ab9865fd654a33c7a22
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-07-02 21:03:09 +1200

    Updated run.py

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
 from app import app
-context = ('domain.crt', 'domain.key')
-app.run(debug=True, ssl_context=context)
+# context = ('domain.crt', 'domain.key')
+
+HOST = '0.0.0.0'
+DEBUG = True
+if __name__ == "__main__":
+    app.run(debug = DEBUG, 
+            host  = HOST)
