979aac2322156b2131b4fceea13dcef05d31389e
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-07-03 19:22:18 +1200

    Added auth_routes logging

--- app/auth_routes.py
+++ app/auth_routes.py
@@ -1,6 +1,8 @@
 from app import app, db, models
 from flask import jsonify, request, session
 
+LOG_PREFIX = 'AUTH:'
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """User register/authentication/session management."""
@@ -11,17 +13,25 @@ def register():
         if not req.get('username')  or not req.get('password'):
             return jsonify({'status': 'failed', 'error': 'Username or/and password missing'})
 
+
         req['username'] = req['username'].lower()
         user = models.Auth.query.filter_by(username=req['username']).first()
 
+
         if not user:
+            logging.debug('%s Registering new user: %s', LOG_PREFIX, req.get('username'))
+            
             new_user = models.Auth(req['username'], req['password'])
             new_user.commit()
             return jsonify({'status': 'success'})
 
         else:
+            logging.debug('%s User %s exists', LOG_PREFIX, req.get('username'))
+
             # If user has no U2F devices
             if user.get_u2f_devices() == []:
+                logging.info('%s Redirecting user, %s, to U2F enrollment', LOG_PREFIX, req.get('username'))
+
                 return jsonify({'status': 'failed', 'u2f_enroll_required': True})
 
             return jsonify({'status': 'failed', 'error': 'User already exists'})
@@ -41,6 +51,7 @@ def login():
         user = models.Auth.query.filter_by(username=req['username']).first()
 
         if user:
+            logging.debug('%s Authenticating user %s', LOG_PREFIX, req.get('username'))
             if user.check_password(req['password']):
                 session['authenticated'] = True
                 session['username']      = user.username
@@ -53,6 +64,7 @@ def login():
             else:
                 return jsonify({'status': 'failed', 'error': 'Username or/and password is incorrect'})
             
+        logging.debug('%s User %s, does not exist ', LOG_PREFIX, req.get('username'))
         return jsonify({'status': 'failed', 'error': 'Username or/and password is incorrect'})
 
     return jsonify({})
@@ -60,12 +72,15 @@ def login():
 @app.route('/islogged')
 def isLogged():
     """User islogged/authentication/session management."""
+
+    logging.debug('%s Checking if user is logged in', LOG_PREFIX) 
     return jsonify({'logged_in': session.get('logged_in', False)})
 
 
 @app.route('/logout')
 def logout():
     """User logout/authentication/session management."""
+    logging.debug('%s Logging out user %s', LOG_PREFIX, session.get('username')) 
     session.pop('logged_in'     , None)
     session.pop('username'      , None)
     session.pop('authenticated' , None)
