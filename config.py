d72a912d9aaa22cd9d12e2e4c02334a28a9a9709
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-07-09 00:19:00 +1200

    :lipstick:

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-import logging
+import os, logging
 
 # ----- LOGGING ----- #
 DEBUG     = True
@@ -54,7 +52,8 @@ USERNAME    = 'admin'
 PASSWORD    = 'admin'
 
 # defines the full path for the database
-DATABASE_PATH = os.path.join(basedir, DATABASE)
+BASEDIR       = os.path.abspath(os.path.dirname(__file__))
+DATABASE_PATH = os.path.join(BASEDIR, DATABASE)
 
 # the database uri
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
