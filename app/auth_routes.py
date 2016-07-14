051ddbfcda0e47c9bb900e0d39e7c8f9735f95c0
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-07-14 19:42:08 +1200

    Migrated to u2f flask-fido-u2f

--- app/auth_routes.py
+++ app/auth_routes.py
@@ -1,4 +1,4 @@
-from app import app, db, models
+from app import app, db, models, u2f
 from flask import jsonify, request, session
 import logging
 
@@ -24,16 +24,10 @@ def register():
             
             new_user = models.Auth(req['username'], req['password'])
             new_user.commit()
-            return jsonify({'status': 'success'})
+            return jsonify({'status': 'ok'})
 
         else:
             logging.debug('%s User %s exists', LOG_PREFIX, req.get('username'))
-
-            # If user has no U2F devices
-            if user.get_u2f_devices() == []:
-                logging.info('%s Redirecting user, %s, to U2F enrollment', LOG_PREFIX, req.get('username'))
-
-                return jsonify({'status': 'failed', 'u2f_enroll_required': True})
 
             return jsonify({'status': 'failed', 'error': 'User already exists'})
 
@@ -57,10 +51,14 @@ def login():
                 session['authenticated'] = True
                 session['username']      = user.username
 
-                if user.get_u2f_devices() == []:
+                if not user.has_u2f_devices():
                     session['logged_in'] = True
-                    return jsonify({'status': 'success'})
+                    session['u2f_enroll_authorized'] = True
+                    session['u2f_allow_device_management']  = True
+
+                    return jsonify({'status': 'ok'})
                 else:
+                    session['u2f_sign_required'] = True
                     return jsonify({'status': 'failed', 'u2f_sign_required': True})
             else:
                 return jsonify({'status': 'failed', 'error': 'Username or/and password is incorrect'})
@@ -75,7 +73,7 @@ def isLogged():
     """User islogged/authentication/session management."""
 
     logging.debug('%s Checking if user is logged in', LOG_PREFIX) 
-    return jsonify({'logged_in': session.get('logged_in', False)})
+    return jsonify({'status' : 'ok', 'logged_in': session.get('logged_in', False)})
 
 
 @app.route('/logout')
@@ -85,4 +83,6 @@ def logout():
     session.pop('logged_in'     , None)
     session.pop('username'      , None)
     session.pop('authenticated' , None)
-    return jsonify({'status': 'success'})
+    u2f.reset_session()
+
+    return jsonify({'status': 'ok'})
