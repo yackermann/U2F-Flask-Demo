f38c6f7ed4064da65eb69de90f3725c580edda37
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-08-22 22:56:24 +1200

    Enabled facets

--- config.py
+++ config.py
@@ -29,9 +29,10 @@ with open(COOKEY_FILE) as r:
 U2F_APPID          = 'https://u2f.jeman.de'
 
 # Set to True to enable facets
-U2F_FACETS_ENABLED = False
+U2F_FACETS_ENABLED = True
 U2F_FACETS_LIST    = [
     'https://u2f.jeman.de'
+    'https://secure.jeman.de'
 ]
 # ----- U2F ENDS ----- #
 
